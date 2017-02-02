b8f5c895439f6f1a79b4844232ee9547099441b4
Chris Hamper <chris.hamper@acquia.com>
2017-02-02 15:48:55 -0500

    Adapt Hello component into Top Nav

---
+++ src/components/TopNav.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="hello">
+    <h1>{{ title }}</h1>
+    <ul>
+      <li><a href="a" target="_blank">Core Docs</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'top-nav',
+  data() {
+    return {
+      title: 'Vuepal',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  ul {
+    display: inline-block;
+    list-style-type: none;
+    padding: 0;
+  }
+
+  h1, li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+    text-decoration: none;
+  }
+</style>
